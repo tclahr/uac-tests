f75aac9ccb956aebea050d5a3a3ad84863c83910
Thiago Canozzo Lahr <tclahr@gmail.com>
2024-08-17 08:32:34 -0300

    add new tests

--- tests/lib/test_get_tools_path.sh
+++ tests/lib/test_get_tools_path.sh
@@ -4,7 +4,7 @@
 
 oneTimeSetUp()
 {
-  . "${UAC_DIR}/lib/get_bin_path.sh"
+  . "${UAC_DIR}/lib/get_tools_path.sh"
 
   _get_system_arch_bin_path()
   {
@@ -53,25 +53,30 @@ oneTimeSetUp()
     esac
   }
 
-  __TEST_TEMP_DIR="${USHUNIT_TEMP_DIR}/test_get_bin_path"
+  __TEST_TEMP_DIR="${USHUNIT_TEMP_DIR}/test_get_tools_path"
 
   mkdir -p "${__TEST_TEMP_DIR}/uac"
-  mkdir -p "${__TEST_TEMP_DIR}/uac/bin"
-  mkdir -p "${__TEST_TEMP_DIR}/uac/bin/aix"
-  mkdir -p "${__TEST_TEMP_DIR}/uac/bin/android"
-  mkdir -p "${__TEST_TEMP_DIR}/uac/bin/esxi"
-  mkdir -p "${__TEST_TEMP_DIR}/uac/bin/netscaler"
-  
+  mkdir -p "${__TEST_TEMP_DIR}/uac/tools/statx"
+  mkdir -p "${__TEST_TEMP_DIR}/uac/tools/statx/android/arm"
+  mkdir -p "${__TEST_TEMP_DIR}/uac/tools/statx/esxi_linux/i686"
+  mkdir -p "${__TEST_TEMP_DIR}/uac/tools/statx/linux"
+  mkdir -p "${__TEST_TEMP_DIR}/uac/tools/statx/netscaler"
+  mkdir -p "${__TEST_TEMP_DIR}/uac/tools/zip"
+  mkdir -p "${__TEST_TEMP_DIR}/uac/tools/zip/freebsd_netscaler"
+  mkdir -p "${__TEST_TEMP_DIR}/uac/tools/zip/solaris"
+
   __UAC_DIR="${__TEST_TEMP_DIR}/uac"
 
 }
 
-test_get_bin_path_success()
+test_get_tools_path_success()
 {
-  __test_actual=`_get_bin_path "aix" "ppc64le"`
-  assertEquals "${__TEST_TEMP_DIR}/uac/bin/aix/ppc64le:${__TEST_TEMP_DIR}/uac/bin/aix:${__TEST_TEMP_DIR}/uac/bin" "${__test_actual}"
-  
-  __test_actual=`_get_bin_path "openbsd" "amd64"`
-  assertEquals "${__TEST_TEMP_DIR}/uac/bin" "${__test_actual}"
+  # TO DO: test zip tool
+  __test_actual=`_get_tools_path "android" "armv6"`
+  assertEquals "${__TEST_TEMP_DIR}/uac/tools/statx/android/arm" "${__test_actual}"
+
+  __test_actual=`_get_tools_path "esxi" "i386"`
+  assertEquals "${__TEST_TEMP_DIR}/uac/tools/statx/esxi_linux/i686" "${__test_actual}"
+
 }
 
